2b8be834a8ad9560664ff938174015d080321189
YordanosT <jordan.esh88@gmail.com>
2025-06-29 23:37:12 +0300

    add Task 4. Full description

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Task 4. Full description
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
