ed0601f62ed628418bdea80e8ffed2004e8a81a3
YordanosT <jordan.esh88@gmail.com>
2025-06-29 23:41:25 +0300

    My commit for task 5. Populating Your Database with Your Very First Data~

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Task5. Populating Your Database with Your Very First Data
+
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
